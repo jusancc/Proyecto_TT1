7da7d000c497974930620e5065530798ba3225c8
Juan Sanchez <jusancc@unirioja.es>
2025-04-30 20:07:37 +0200

    iteracion 1

---
+++ include/NutAngles.hpp
@@ -0,0 +1,12 @@
+#ifndef _NUT_ANGLES_
+#define _NUT_ANGLES_
+
+#include "SAT_Const.hpp"
+#include <iostream>
+#include <cmath>
+#include <tuple>
+using namespace std;
+
+tuple<double,double> NutAngles(double Mjd_TT);
+
+#endif
